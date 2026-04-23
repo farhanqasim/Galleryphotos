d82a9d743a9f60e91d1b2219b304492f4a75a2e0
farhanqasim <farhanqasim854@gmail.com>
2026-04-23 19:15:37 +0500

    Gallery project

--- src/components/FolderCard.jsx
+++ src/components/FolderCard.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 const FolderCard = ({ folder }) => {
   return (
-    <Link to={`/folders/${folder.id}`} className="folder-card">
-      <div className="folder-icon"><img src={'https://www.thefinalstep.co.uk/hs-fs/hubfs/Screenshot%202022-05-04%20at%2015.04.03.png?width=1479&name=Screenshot%202022-05-04%20at%2015.04.03.png'} alt="folder icon" width={150} height={100} /></div>
-      <h3 style={{ fontSize: '14px', fontWeight: 'bold' ,textTransform: 'capitalize'}}>{folder.name || folder.title || `Folder ${folder.id}`}</h3>
+    <Link to={`/folders/${folder.id}`} className="folder-card" style={{ textTransform: 'capitalize'}}>
+      <div className="folder-icon" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}} ><img src={'https://www.thefinalstep.co.uk/hs-fs/hubfs/Screenshot%202022-05-04%20at%2015.04.03.png?width=1479&name=Screenshot%202022-05-04%20at%2015.04.03.png'} alt="folder icon" width={150} height={100} /></div>
+      <h3 style={{ textTransform: 'capitalize', textAlign: 'center'}} >{folder.name || folder.title || `Folder ${folder.id}`}</h3>
       {/* <p>Open folder images</p> */}
     </Link>
   )
